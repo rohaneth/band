0d2b8c70200f35619416afbc9883eae83eb6f5a0
rohaneth <148790280+rohaneth@users.noreply.github.com>
2023-10-24 22:46:18 +0530

    Update index.js
    keybord shortcut

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+var audio = [
+  new Audio("sounds/tom-1.mp3"),
+  new Audio("sounds/tom-2.mp3"),
+  new Audio("sounds/tom-3.mp3"),
+  new Audio("sounds/tom-4.mp3"),
+  new Audio("sounds/snare.mp3"),
+  new Audio("sounds/crash.mp3"),
+  new Audio("sounds/kick-bass.mp3"),
+  new Audio("sounds/tom-1.mp3")
+      ]
 for(let i = 0; i<7;i++){
 document.querySelectorAll("button")[i].addEventListener("click" , function (){
     // var audio0 = new Audio("sounds/tom-1.mp3");
@@ -10,16 +19,6 @@ document.querySelectorAll("button")[i].addEventListener("click" , function (){
     // var audio6 = new Audio("sounds/kick-bass.mp3");
     // var audio7 = new Audio("sounds/tom-1.mp3");
         
-        var audio = [
-    new Audio("tom-1.mp3"),
-    new Audio("tom-2.mp3"),
-    new Audio("tom-3.mp3"),
-    new Audio("tom-4.mp3"),
-    new Audio("snare.mp3"),
-    new Audio("crash.mp3"),
-    new Audio("kick-bass.mp3"),
-    new Audio("tom-1.mp3")
-        ]
     
       //audio.play();
 
@@ -30,5 +29,35 @@ document.querySelectorAll("button")[i].addEventListener("click" , function (){
       //console.log(this);
       //this.style.color = "white";
       
+      
 });
+
 }
+document.addEventListener("keydown",(event) =>{
+
+  switch (event.key){
+    case "w":
+    audio[0].play();
+    break;
+    case "a":
+    audio[1].play();
+    break;
+    case "s":
+    audio[2].play();
+    break;
+    case "d":
+    audio[3].play();
+    break;
+    case "j":
+    audio[4].play();
+    break;
+    case "k":
+    audio[5].play();
+    break;
+    case "l":
+    audio[6].play();
+    break;
+    
+  }
+});
+
