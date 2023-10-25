8588e1fb5a6e98f01f7772420eef66ef318ed43d
rohaneth <148790280+rohaneth@users.noreply.github.com>
2023-10-25 14:04:44 +0530

    Update index.js
    Added  button click css

--- index.js
+++ index.js
@@ -10,28 +10,16 @@ var audio = [
       ]
 for(let i = 0; i<7;i++){
 document.querySelectorAll("button")[i].addEventListener("click" , function (){
-    // var audio0 = new Audio("sounds/tom-1.mp3");
-    // var audio1 = new Audio("sounds/tom-2.mp3");
-    // var audio2 = new Audio("sounds/tom-3.mp3");
-    // var audio3 = new Audio("sounds/tom-4.mp3");
-    // var audio4 = new Audio("sounds/snare.mp3");
-    // var audio5 = new Audio("sounds/crash.mp3");
-    // var audio6 = new Audio("sounds/kick-bass.mp3");
-    // var audio7 = new Audio("sounds/tom-1.mp3");
-        
-    
-      //audio.play();
-
+  
       if(this == document.querySelectorAll("button")[i] ){
          audio[i].play();
-        // document.querySelectorAll("button")[i].className = "clkbtn";
+         document.querySelectorAll("button")[i].classList.add("pressed");
+         setTimeout(()=>{
+          document.querySelectorAll("button")[i].classList.remove("pressed");
+         },100);
       }
-      //console.log(this);
-      //this.style.color = "white";
-      
       
 });
-
 }
 document.addEventListener("keydown",(event) =>{
 
@@ -39,6 +27,7 @@ document.addEventListener("keydown",(event) =>{
     case "w":
     audio[0].play();
     break;
+
     case "a":
     audio[1].play();
     break;
@@ -57,7 +46,17 @@ document.addEventListener("keydown",(event) =>{
     case "l":
     audio[6].play();
     break;
-    
+  }
+  buttonAnimation(event);
+
+  function buttonAnimation(event){
+    // var activeButton = document.querySelector("." + event.key);
+    // activeButton.classList.add("pressed");
+    document.querySelector("." + event.key).classList.add("pressed");
+
+    setTimeout(()=>{
+      document.querySelector("." +event.key).classList.remove("pressed");
+    },100);
   }
 });
 
